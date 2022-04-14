ad58c03b7622afb3bb8de4cafe7f7ba2d49231e6
krzm <kmazanek@gmail.com>
2022-04-14 16:25:15 +0200

    Add msg bus client

---
+++ PlatformService/AsyncDataServices/MessageBusClient.cs
@@ -0,0 +1,84 @@
+using System.Text;
+using System.Text.Json;
+using PlatformService.Dtos;
+using RabbitMQ.Client;
+
+namespace PlatformService.AsyncDataServices;
+
+public class MessageBusClient :
+    IMessageBusClient
+{
+    private readonly IConfiguration config;
+    private readonly IConnection? connection;
+    private readonly IModel? channel;
+
+    public MessageBusClient(
+        IConfiguration config
+    )
+    {
+        this.config = config;
+        var factory = new ConnectionFactory()
+        {
+            HostName = config["RabbitMQHost"]
+            , Port = int.Parse(config["RabbitMQHost"])
+        };
+        try
+        {
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+
+            connection.ConnectionShutdown += RabbitMQConnectionShutdown;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"--> Could not connect to the Message Bus: {ex.Message}");
+        }
+    }
+
+    private void RabbitMQConnectionShutdown(object? sender, ShutdownEventArgs e)
+    {
+        Console.WriteLine($"--> RabbitMQ Connection Shutdown");
+    }
+
+    public void PublishNewPlatform(
+        PlatformPublishedDto platform)
+    {
+        var msg = JsonSerializer.Serialize(platform);
+        ArgumentNullException.ThrowIfNull(connection);
+        if(connection.IsOpen)
+        {
+            Console.WriteLine($"--> RabbitMQ Connection Open, sending message...");
+            SendMessage(msg);
+        }
+        else
+        {
+            Console.WriteLine($"--> RabbitMQ Connection closed, not sending");
+        }
+    }
+
+    private void SendMessage(string message)
+    {
+        var body = Encoding.UTF8.GetBytes(message);
+        ArgumentNullException.ThrowIfNull(channel);
+        channel.BasicPublish(
+            exchange: "trigger"
+            , routingKey: ""
+            , basicProperties: null
+            , body: body);
+        Console.WriteLine($"--> We have sent {message}");
+    }
+
+    public void Dispose()
+    {
+        ArgumentNullException.ThrowIfNull(channel);
+        ArgumentNullException.ThrowIfNull(connection);
+        if(channel.IsOpen)
+        {
+            channel.Close();
+            connection.Close();
+        }
+        Console.WriteLine("MessageBus Disposed");
+    }
+}
--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -8,8 +8,8 @@ public class PlatformsProfile : Profile
 {
     public PlatformsProfile()
     {
-        // Source -> Target
         CreateMap<Platform, PlatformReadDto>();
         CreateMap<PlatformCreateDto, Platform>();
+        CreateMap<PlatformReadDto, PlatformPublishedDto>();
     }
 }
